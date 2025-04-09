f75f53daa824811a817caa63628deb7296cd238c
Youseef Tareq <youseeftareq5176@gmail.com>
2025-04-09 11:37:48 +0200

    feat: Enhance home page layout and add section header component
    - Added new styles for section1, including image and text formatting.
    - Introduced SectionHeaderComponent for better section titles.
    - Updated HeaderComponent with a responsive mobile menu.
    - Added multiple new images for content enhancement.
    - Included Font Awesome for icon support in the header.
    - Cleaned up font-face definitions in styles.scss.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
+import { SectionHeaderComponent } from "../../shared/components/section-header/section-header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SectionHeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
