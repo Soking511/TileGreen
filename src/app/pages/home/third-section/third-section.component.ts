ba57976fb12377fc514ed09c1c938f749dd539ae
Youseef Tareq <youseeftareq5176@gmail.com>
2025-04-17 13:17:19 +0200

    feat: Enhance layout and typography across multiple sections, add new fonts, and restructure header navigation

--- src/app/pages/home/third-section/third-section.component.ts
+++ src/app/pages/home/third-section/third-section.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SectionHeaderComponent } from "../../../shared/components/section-header/section-header.component";
 
 @Component({
   selector: 'app-third-section',
-  imports: [],
+  imports: [SectionHeaderComponent],
   templateUrl: './third-section.component.html',
 })
 export class ThirdSectionComponent {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}"
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        'neue-haas': ['"Neue Haas Display"', 'sans-serif'],
-        'inter-display': ['"Inter Display"', 'sans-serif'],
+        sans: ['CustomFont', 'sans-serif'],
       },
-      fontWeight: {
-        'light': 500,
-        'black': 900
-      }
     },
   },
   plugins: [],
