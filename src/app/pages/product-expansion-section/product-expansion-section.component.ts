98ed8192afc321113269b9aa5dcb8334fe8ff1dd
Youseef Tareq‬‏ <63275823+Soking511@users.noreply.github.com>
2025-05-05 10:13:28 +0300

    Refactor layout and styling across multiple components for improved responsiveness and consistency
    - Updated padding and margin values in about, careers, home, and technology pages for better alignment.
    - Enhanced typography and spacing in the fourth section and third section of the home page.
    - Introduced a new product card component for better modularity and reusability.
    - Improved image handling and hover effects in product expansion section.
    - Adjusted global text styles for better readability and consistency.
    - Fixed minor issues in SVG rendering and layout alignment.

--- src/app/pages/product-expansion-section/product-expansion-section.component.ts
+++ src/app/pages/product-expansion-section/product-expansion-section.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from '../../shared/components/product-card/product-card.component';
+
+interface ProductCard {
+  imageUrl: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-product-expansion-section',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './product-expansion-section.component.html',
-  styleUrls: ['./product-expansion-section.component.scss'],
 })
 export class ProductExpansionSectionComponent {
-
+  products: ProductCard[] = [
+    {
+      imageUrl:
+        'https://api-tilegreen.pulslytics.agency/media/images/rectangle-12.png',
+      title: 'Roofing tiles',
+    },
+    {
+      imageUrl:
+        'https://api-tilegreen.pulslytics.agency/media/images/rectangle-12-4.png',
+      title: 'Outdoor Furniture',
+    },
+    {
+      imageUrl:
+        'https://api-tilegreen.pulslytics.agency/media/images/rectangle-12-3.png',
+      title: 'Cladding Sheets',
+    },
+    {
+      imageUrl:
+        'https://api-tilegreen.pulslytics.agency/media/images/rectangle-12-2.png',
+      title: 'Panels',
+    },
+  ];
 }
